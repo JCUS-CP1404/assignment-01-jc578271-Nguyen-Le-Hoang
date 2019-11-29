e08910a043ec5f2e2d08b1a7e24eec63b2d4b478
Nguyen Le Hoang <lehoang.nguyen@my.jcu.edu.au>
2019-11-29 22:43:18 +0800

    second commit

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,31 @@
 """
-Replace the contents of this module docstring with your own details
-Name:
-Date started:
-GitHub URL:
+Name: Nguyen Le Hoang
+Date started: 29/11/2019
+GitHub URL: https://github.com/jc578271/assignment1.git
 """
 
 
 def main():
     """..."""
-    print("Movies To Watch 1.0 - by <Your Hoang>")
+    print("Movies To Watch 1.0 - by <Nguyen Le Hoang>")
+
+    print("""
+    Menu:
+    L - List movies
+    A - Add new movie
+    W - Watch movie
+    Q - Quit
+    """)
+    while True:
+        choiceInMenu = input(">>> ").upper()
+        if choiceInMenu == "L":
+            listMovie()
+
+
+def listMovie():
+    in_file = open('movies.csv' ,'r')
+
+    print(in_file)
 
 
 if __name__ == '__main__':
